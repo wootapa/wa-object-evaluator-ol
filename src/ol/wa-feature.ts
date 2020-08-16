121be225c1308be38a167b96a3be27b132ea4f5c
Andreas Petersson <apesv03@gmail.com>
2020-08-16 23:39:41 +0200

    Fix multi matching

--- src/ol/wa-feature.ts
+++ src/ol/wa-feature.ts
@@ -256,7 +256,7 @@ export class WAFeature {
             return this.getGeometry().intersectsCoordinate(feature.getCenter());
         }
         // Turf contains cant understand multigeometries so we map...
-        return this.asTurfArray(projCode).every(a => feature.asTurfArray(projCode).every(b => booleanContains(a, b)));
+        return this.asTurfArray(projCode).some(a => feature.asTurfArray(projCode).some(b => booleanContains(a, b)));
     }
 
     dwithin(feature: WAFeature, distance: number, projCode: string): boolean {
